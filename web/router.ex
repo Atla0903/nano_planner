b9fd27bb04480fa36f066d770be25029274c0a60
新井達也 <s132813@gmail.com>
2019-01-05 09:49:00 +0900

    Initial commit

---
+++ web/router.ex
@@ -0,0 +1,17 @@
+defmodule NanoPlanner.Router do
+  use NanoPlanner.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", NanoPlanner do
+    pipe_through :browser
+
+    get "/", TopController, :index
+  end
+end
